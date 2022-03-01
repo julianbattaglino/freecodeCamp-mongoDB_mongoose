0ad3aeed79ecd887cf481078a6d024f1ab88157e
Julian Battaglino <julianbattaglino@gmail.com>
2022-03-01 12:42:16 -0300

    schema

--- myApp.js
+++ myApp.js
@@ -60,14 +60,15 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 
 /* 2 - Create a Model */
-const { Schema } = mongoose;
-const personSchema = new Schema({
+var Schema  = mongoose.Schema;
+
+var personSchema = new Schema({
   name:  { type: String, required: true },
   age: Number,
   favoriteFoods:  [String],
 })
 
-const Person = mongoose.model('Person', personSchema);
+var Person = mongoose.model('Person', personSchema);
 
 
 
