1d01bffeef525b2f6bfddea5fff7758772ad6621
Julian Battaglino <julianbattaglino@gmail.com>
2022-03-01 12:24:45 -0300

    model

--- myApp.js
+++ myApp.js
@@ -53,10 +53,16 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
-/* CONNECT MONGODB & MOONGOSE*/
+/* 1 - CONNECT MONGODB & MOONGOSE*/
 const mySecret = process.env['MONGO_URI']
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+/* 2 - Create a Model */
+
+
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
